f6a15caa9656ab3596a64df5eccf4bba7ab2d0f2
ldngduong <letungduong1624@gmail.com>
2025-04-07 03:34:53 +0700

    fix top bar

--- frontend/src/components/common/Topbar.jsx
+++ frontend/src/components/common/Topbar.jsx
@@ -4,7 +4,7 @@ import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 const Topbar = () => {
   return (
-    <div className='flex justify-between md:hidden sticky bg-gray-50 shadow-lg top-0 px-6 py-3 text-gray-600'>
+    <div className='flex justify-between md:hidden sticky bg-gray-50 shadow-lg top-0 px-6 py-3 text-gray-600 z-50'>
       <div className="h-8 w-8 flex gap-1 items-center">
         <img src={logo} className='opacity-65 w-full h-full' alt="" />
         <p className='font-bold text-2xl'>sharre!</p>

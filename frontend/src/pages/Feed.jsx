6ac4541ae32a5178f12e5b6738268600bde690bf
ldngduong <letungduong1624@gmail.com>
2025-04-08 18:34:21 +0700

    fix glitch

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 import PostButton from '../components/post/PostButton';
 import PostContainer from '../components/post/PostContainer';
+import usePostStore from '../store/postStore';
 
 const Feed = () => {
     const [activeTab, setActiveTab] = useState('following');
+    const {feedLoading, feedLoadingMore} = usePostStore()
 
     return (
         <div className="text-gray-600 min-h-screen w-full p-2 md:p-5 bg-gray-50 flex flex-col items-center gap-5">
             <div role="tablist" className="tabs tabs-box">
-                <a 
+                <button 
+                    disabled={feedLoadingMore}
                     role="tab" 
-                    className={`tab ${activeTab === 'following' ? 'tab-active' : ''}`}
+                    className={` tab ${activeTab === 'following' ? 'tab-active' : ''} cursor-pointer`}
                     onClick={() => setActiveTab('following')}
                 >
                     Đang theo dõi
-                </a>
-                <a 
+                </button>
+                <button 
+                    disabled={feedLoadingMore}
                     role="tab" 
-                    className={`tab ${activeTab === 'explore' ? 'tab-active' : ''}`}
+                    className={`tab ${activeTab === 'explore' ? 'tab-active' : ''} cursor-pointer`}
                     onClick={() => setActiveTab('explore')}
                 >
                     Khám phá
-                </a>
+                </button>
             </div>
             <PostButton />
             <PostContainer type={activeTab} />
